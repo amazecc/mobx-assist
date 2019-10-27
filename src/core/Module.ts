c5cd55f3d685a77a4226495e36569eb456376193
Alex <cls1874@163.com>
2019-10-27 10:24:32 +0800

    修复 Module.setState 类型问题

--- src/core/Module.ts
+++ src/core/Module.ts
@@ -20,13 +20,8 @@ export class Module<S extends object, G extends object = {}> {
         stores.add(moduleName, this.state);
     }
 
-    protected setState(value: Partial<S> | ((state: S) => void), actionName?: string) {
-        const fn =
-            typeof value === "function"
-                ? () => value(this.state)
-                : () => {
-                      Object.keys(value).forEach(_ => (this.state[_] = value[_]));
-                  };
+    protected setState<K extends keyof S>(value: Pick<S, K> | ((state: S) => void), actionName?: string) {
+        const fn = typeof value === "function" ? () => value(this.state) : () => Object.keys(value).forEach(_ => (this.state[_] = value[_]));
         if (actionName) {
             action(actionName, fn)();
         } else {
@@ -37,7 +32,7 @@ export class Module<S extends object, G extends object = {}> {
     public resetState(skipFields: Array<keyof S> = [], actionName?: string) {
         const omitComputedFieldsStateKeys = Object.keys(toJS(this.state));
         const keys = skipFields && skipFields.length > 0 ? omitComputedFieldsStateKeys.filter(_ => !((skipFields as unknown) as string[]).includes(_)) : omitComputedFieldsStateKeys;
-        const finalState: Partial<S> = keys.reduce((prev, next) => {
+        const finalState = keys.reduce((prev, next) => {
             prev[next] = this.initialState[next];
             return prev;
         }, {});
