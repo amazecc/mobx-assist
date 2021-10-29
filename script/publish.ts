867ee140060da7c96e67f75ea6363900d9bb3e9c
Alex <cls1874@163.com>
2021-10-29 14:43:50 +0800

    更新发布脚本

--- script/publish.ts
+++ script/publish.ts
@@ -1,4 +1,3 @@
-import "zx/globals";
 import "colors";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
@@ -6,10 +5,12 @@ import { exec } from "./utils";
 
 const argv = yargs(hideBin(process.argv)).argv as any;
 
-const type = argv._[0] as "patch" | "minor" | "major";
+export const type = argv._[0] as "patch" | "minor" | "major";
 
 console.log("编译打包".green);
-exec("rm -rf lib && tsc -p tsconfig.build.json && babel lib --out-dir lib");
+exec("rm -rf lib"); // 删除旧文件
+exec("tsc -p tsconfig.build.json") // 编译生成 js 文件，类型声明文件
+exec("babel lib --out-dir lib") // es6 -> es5, 添加 polyfills
 
 console.log("设置新的版本号".green);
 exec(`npm version ${type}`);
